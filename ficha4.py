9d96377f8ee8ba8d889e94ee41c304dcecf9a9ba
pedro ramos <pedro.ramos9@hotmail.com>
2022-04-04 15:40:26 +0100

    ficha 6 terminada

--- ficha4.py
+++ ficha4.py
@@ -1,21 +1,4 @@
 def ex1(str):
-    # a=0
-    # e=0
-    # i=0
-    # o=0
-    # u=0
-    # for letter in str:
-    #     if letter=="a":
-    #         a+=1
-    #     elif letter=="e":
-    #         e+=1
-    #     elif letter=="i":
-    #         i+=1
-    #     elif letter=="o":
-    #         o+=1
-    #     elif letter=="u":
-    #         u+=1
-    # return "Ocorrências da Vogal a: ",a,"Ocorrências da Vogal e: ",e,"Ocorrências da Vogal i: ",i,"Ocorrências da Vogal o: ",o,"Ocorrências da Vogal u: ",u
     print("Ocorrências da Vogal a: ",str.count("a"))
     print("Ocorrências da Vogal a: ",str.count("e"))
     print("Ocorrências da Vogal a: ",str.count("i"))
--- ficha6.py
+++ ficha6.py
@@ -1,4 +1,4 @@
-
+import math
 soma = 0
 i = 20
 for k in range(i, 0, -2):
@@ -9,4 +9,20 @@ lista1 = ["Iva", 25, "Rui", 19, "Rato", "abc", 33]
 
 print(lista1)
 
-print(lista1[2])
+print(lista1[2:5])
+print(lista1[:3])   
+print(lista1[3:])   
+lista2= ["jose", 25, "catia", 19, "gato", "xyz", 33]
+lista2.append(lista1)
+print(lista2)
+
+
+def ex3_esfera(r):
+    return (4*math.pi*math.pow(r,3))/3
+
+print(ex3_esfera(5))
+
+a=int(input("digite um numero: "))
+
+for k in range(1,a+1):
+    print("*"*k)
