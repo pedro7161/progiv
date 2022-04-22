fdc23a5c046b416dabd66dd9c1e6330163dac5b7
pedro7161 <pedro.ramos9@hotmail.com>
2022-04-22 11:49:08 +0100

    ficha 11

---
+++ ficha11.py
@@ -0,0 +1,14 @@
+def separator(t):
+    arr = t.split(" ")
+    unique(arr)
+    return arr
+
+
+def unique(arr):
+    for word in arr:
+
+        if str.find(word)
+
+
+text = input("digite uma frase: ")
+print(separator(text))
