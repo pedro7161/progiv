fdd15486f669841408526fedf5c3b86b1da1c880
pedro7161 <pedro.ramos9@hotmail.com>
2022-05-06 13:17:01 +0100

    ficha 11 e 12

--- ficha11.py
+++ ficha11.py
@@ -17,11 +17,10 @@ def unique(arr):
             uniquelist.append(word)
             positionuniquedic[word] = pos
 
-        else:
-            positionsaver(word, pos, positiondic)
+        positionsaver(word, pos, positiondic)
         pos = pos+1
 
-    return constructor(positiondic, uniquelist)
+    return constructor(positiondic)
 
 
 def positionsaver(word, pos, positiondic):
@@ -38,27 +37,61 @@ def positionsaver(word, pos, positiondic):
     return positiondic
 
 
-def constructor(positiondic, uniquelist):
-    complete = []
-    sorting = []
+def constructor(positiondic):
+    sorted = []
+    sentence = []
+
+    for k, v in positiondic.items():  # poque tens isto aqui? para adicionar as posiçoes dos array todos num array e meter sort,
+        # a minha ideia é comparar as posiçoes ha posiçao dentro do diçionario com o array de posiçoes e trocar esses numeros iguais pela palavra
+        # nao se se é nessesario
+        # im dumb é a unica cena que vem ha cabeça
+        # substituir os numeros pelas palavras,eu é que buguei agora na substituiçao
+        for i in v:
+            sorted.append(i)
+
+        sorted.sort()
+
+        """ 
+            {
+            "ola":[1,2],
+            "tudo":[3,4]
+            }
+        
+        sorted=[0,1,2,3,4,5] dont need this o pc sabe contar
+        
+        para numero in sorted
+            se o numero tiver no dicionario[array] 
+                trocar o numero pela palvavra associada no dicionario
+                
+        """
+
+    finalstring = ""
+    # EStou a pensar
+    # tou atento ha ala
 
     for k, v in positiondic.items():
-        for position in v:
-            sorting.append(position)
-            sorting.sort()
-            print(sorting)
+        for number in sorted:
+            for i in range(len(k)):
+                print(positiondic[k][i])
+                if number == positiondic[k][i]:
+                    sentence.append(positiondic[v])
+    # for k, v in positiondic.items():
+    #     for position in v:
+    #         sorting.append(position)
+    #         sorting.sort()
+    #         print(sorting)
 
-        for position in range(len(uniquelist)):
-            for l in v:
-                for j in range(len(sorting)):
-                    print(j, sorting[j], position,
-                          uniquelist[position], l, v)
-                    if position < sorting[j]:
-                        complete.append(uniquelist[position])
-                    else:
-                        complete.append(positiondic[k][j])
+    #     for position in range(len(uniquelist)):
+    #         for l in v:
+    #             for j in range(len(sorting)):
+    #                 print(j, sorting[j], position,
+    #                       uniquelist[position], l, v)
+    #                 if position < sorting[j]:
+    #                     complete.append(uniquelist[position])
+    #                 else:
+    #                     complete.append(positiondic[k][j].value)
 
-    return complete
+    return sentence
 
 
 text = input("digite uma frase: ")
