d656cb191a016992c117bf22fafdba8377e7d9ce
pedro7161 <pedro.ramos9@hotmail.com>
2022-05-13 09:30:15 +0100

    ficha 12 completa

--- ficha12.py
+++ ficha12.py
@@ -8,12 +8,33 @@ words_t = []
 try:
     with open('ficha12.txt') as f:
         lines = f.readlines()
-
-
+    f.close()
 except:
     print('Problema na abertuta do ficheiro')
 
+print(lines)
+for text in lines:
+    str = text
 
+    vogais += str.count("a")+str.count("e") + \
+        str.count("i")+str.count("o")+str.count("u")
+
+    words_t.append(str.split(" "))
+
+    for k in range(len(str)):
+        if str[k].isalpha():
+            count += 1
+for line in range(len(words_t)):
+    for word in range(len(words_t[line])):
+        if words_t[line][word] != "\n":
+            Twords += 1
+        else:
+            continue
+
+print("vogais: ", vogais)
+print("consoantes: ", count-vogais)
+print("palavras: ", Twords)
+print("linhas: ", len(lines))
 # ex2
 words_t = []
 unique = []
@@ -26,12 +47,12 @@ except:
 
 for text in lines:
     str = text
-
-    words_t.append(str.split(" "))
+    words_t.append(str.split(None))
 
 for line in range(len(words_t)):
     for word in range(len(words_t[line])):
-        if words_t[line][word] != "\n" and words_t[line][word] not in unique:
+
+        if words_t[line][word] != " " and words_t[line][word] not in unique:
             unique.append(words_t[line][word])
         else:
             continue
@@ -42,16 +63,10 @@ try:
     time.sleep(5)
     f.close()
 except:
-    print('Problema na criaçao do ficheiro')
+    print('Problema na criaçao do ficheiro or file already exists')
 
 
-try:
-    with open('ficha12_newtext.txt', 'r+') as f:
-        lines = f.readlines()
-
-except:
-    print('Problema na abertuta do ficheiro')
-
-for word in range(len(unique)):
-    f.write(unique[word])
-f.close()
+with open('ficha12_newtext.txt', 'r+') as f:
+    for word in range(len(unique)):
+        f.write(unique[word])
+        f.write("\n")
