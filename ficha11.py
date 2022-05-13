a615d33135108adb7abfea63d8f4d2e4ac88293f
pedro7161 <pedro.ramos9@hotmail.com>
2022-05-13 08:55:21 +0100

    ficha 11 terminada

--- ficha11.py
+++ ficha11.py
@@ -1,7 +1,6 @@
 
 def separator(t):
     arr = t.split(" ")
-    # positionsaver(arr)
     return unique(arr)
 
 
@@ -19,7 +18,7 @@ def unique(arr):
 
         positionsaver(word, pos, positiondic)
         pos = pos+1
-
+    print("unique words: ", uniquelist)
     return constructor(positiondic)
 
 
@@ -41,56 +40,17 @@ def constructor(positiondic):
     sorted = []
     sentence = []
 
-    for k, v in positiondic.items():  # poque tens isto aqui? para adicionar as posiçoes dos array todos num array e meter sort,
-        # a minha ideia é comparar as posiçoes ha posiçao dentro do diçionario com o array de posiçoes e trocar esses numeros iguais pela palavra
-        # nao se se é nessesario
-        # im dumb é a unica cena que vem ha cabeça
-        # substituir os numeros pelas palavras,eu é que buguei agora na substituiçao
+    for k, v in positiondic.items():
         for i in v:
             sorted.append(i)
 
         sorted.sort()
 
-        """ 
-            {
-            "ola":[1,2],
-            "tudo":[3,4]
-            }
-        
-        sorted=[0,1,2,3,4,5] dont need this o pc sabe contar
-        
-        para numero in sorted
-            se o numero tiver no dicionario[array] 
-                trocar o numero pela palvavra associada no dicionario
-                
-        """
-
-    finalstring = ""
-    # EStou a pensar
-    # tou atento ha ala
-
-    for k, v in positiondic.items():
-        for number in sorted:
-            for i in range(len(k)):
-                print(positiondic[k][i])
+    for number in sorted:
+        for k, v in positiondic.items():
+            for i in range(len(v)):
                 if number == positiondic[k][i]:
-                    sentence.append(positiondic[v])
-    # for k, v in positiondic.items():
-    #     for position in v:
-    #         sorting.append(position)
-    #         sorting.sort()
-    #         print(sorting)
-
-    #     for position in range(len(uniquelist)):
-    #         for l in v:
-    #             for j in range(len(sorting)):
-    #                 print(j, sorting[j], position,
-    #                       uniquelist[position], l, v)
-    #                 if position < sorting[j]:
-    #                     complete.append(uniquelist[position])
-    #                 else:
-    #                     complete.append(positiondic[k][j].value)
-
+                    sentence.append(k)
     return sentence
 
 
--- ficha12.py
+++ ficha12.py
@@ -8,33 +8,12 @@ words_t = []
 try:
     with open('ficha12.txt') as f:
         lines = f.readlines()
-    f.close()
+
+
 except:
     print('Problema na abertuta do ficheiro')
 
-print(lines)
-for text in lines:
-    str = text
 
-    vogais += str.count("a")+str.count("e") + \
-        str.count("i")+str.count("o")+str.count("u")
-
-    words_t.append(str.split(" "))
-
-    for k in range(len(str)):
-        if str[k].isalpha():
-            count += 1
-for line in range(len(words_t)):
-    for word in range(len(words_t[line])):
-        if words_t[line][word] != "\n":
-            Twords += 1
-        else:
-            continue
-
-print("vogais: ", vogais)
-print("consoantes: ", count-vogais)
-print("palavras: ", Twords)
-print("linhas: ", len(lines))
 # ex2
 words_t = []
 unique = []
@@ -67,7 +46,7 @@ except:
 
 
 try:
-    with open('ficha12_newtext.txt', 'r') as f:
+    with open('ficha12_newtext.txt', 'r+') as f:
         lines = f.readlines()
 
 except:
